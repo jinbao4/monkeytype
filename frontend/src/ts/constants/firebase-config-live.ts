afa1d6351e0db2656675461a072acc315ec88e89
stranger <128334043+jinbao4@users.noreply.github.com>
2026-04-21 19:20:10 +0100

    Update and rename firebase-config-example.ts to firebase-config-live.ts

---
+++ frontend/src/ts/constants/firebase-config-live.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDMNrROpHPKHT3Yc9fauyEMYQUboHqQcLs",
+  authDomain: "idk2-6b329.firebaseapp.com",
+  projectId: "idk2-6b329",
+  storageBucket: "idk2-6b329.firebasestorage.app",
+  messagingSenderId: "521421476755",
+  appId: "1:521421476755:web:6a3b7425858364c2d10e66",
+  measurementId: "G-GRNE9JQ6KH"
+};
